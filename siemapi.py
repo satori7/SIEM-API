c51c168f0006adc2102386b523302648f0507f6f
Chris Hawkins <satori7@gmail.com>
2019-05-13 15:44:12 -0500

    Mostly working now. Just need to add the what datasources and how many thingy.

--- siemapi.py
+++ siemapi.py
@@ -1,17 +1,26 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 
 import sys
 import requests
 import json
 import urllib3
+import base64
+import getpass
+
+esmuser = input("Username: ");
+esmpass = getpass.getpass(prompt="Password: ");
+esmip = input("ESM IP: ");
 
 # Config options: URL to connect to, send calls to, and user/pass
-authUrl = 'https://10.57.12.95/rs/esm/login/'
-url = 'https://10.57.12.95/rs/esm/v2/'
-# Uncomment for apiuser
-# authBody = { "username": "YXBpdXNlcgo=", "password": "U2VjdXJpdHkuNHU=", "locale": "en_US" }
-# Uncomment for NGCP
-authBody = { "username": "TkdDUA==", "password": "U2VjdXJpdHkuNHU=", "locale": "en_US" }
+authUrl = "https://{}/rs/esm/login/".format(esmip);
+url = "https://{}/rs/esm/v2/".format(esmip);
+
+esmuser = base64.b64encode(esmuser.encode('utf-8'));
+esmuser = esmuser.decode('utf-8')
+esmpass = base64.b64encode(esmpass.encode('utf-8'));
+esmpass = esmpass.decode('utf-8')
+authBody = { "username": "{}".format(esmuser), "password": "{}".format(esmpass), "locale": "en_US" }
 
 # Silence the annoying insecure warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -41,21 +50,19 @@ parsedJson2 = r2.json()
 resultID2 = parsedJson2['resultID']
 print (resultID2);
 
-getres = 'qryGetResults?startPos=0&numRows=500&reverse=false'
+getres = 'qryGetResults?startPos=0&numRows=10&reverse=false'
 data3 = { "resultID": resultID2 }
 
-r4 = client.post(url+getres, headers=headers, json=data3)
-print(r4.text)
+r3 = client.post(url+getres, headers=headers, json=data3)
+print(r3.text)
 
 close = 'qryClose?resultID='+resultID2
-r4 = client.post(url+close, headers=headers)
+r3 = client.post(url+close, headers=headers)
 
 print ("\n\n*** Random debug stuff ***")
 print("URL:", url+call)
 print("URL2:", url+getres)
 print('Result ID from r2:', resultID2)
-# print('Result ID from r3:', resultID3)
 print("Status1:", r1.status_code)
 print("Status2:", r2.status_code)
-# print("Status3:", r3.status_code)
-print("Status4:", r4.status_code)
+print("Status3:", r3.status_code)
