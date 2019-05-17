c63e8ddf7459f6423d808856153bdfd9b4649521
Chris Hawkins <satori7@gmail.com>
2019-05-17 15:29:25 -0500

    The loop for qryGetStatus works now.

--- siemapi.py
+++ siemapi.py
@@ -7,10 +7,16 @@ import json
 import urllib3
 import base64
 import getpass
+import time
 
 esmuser = input("Username: ");
 esmpass = getpass.getpass(prompt="Password: ");
 esmip = input("ESM IP: ");
+
+# For testing
+esmuser = 'NGCP'
+esmpass = 'Security.4u'
+esmip = '10.57.12.95'
 
 # Config options: URL to connect to, send calls to, and user/pass.
 authUrl = "https://{}/rs/esm/login/".format(esmip);
@@ -20,6 +26,7 @@ esmuser = base64.b64encode(esmuser.encode('utf-8'));
 esmuser = esmuser.decode('utf-8')
 esmpass = base64.b64encode(esmpass.encode('utf-8'));
 esmpass = esmpass.decode('utf-8')
+
 authBody = { "username": "{}".format(esmuser), "password": "{}".format(esmpass), "locale": "en_US" }
 
 # Silence the annoying insecure warning.
@@ -60,17 +67,21 @@ resultID2 = parsedJson2['resultID']
 # Check to see if the job is done.
 statusCall = "qryGetStatus"
 data4 = { "resultID": { "{}".format(resultID2) }}
-try:
-    r4 = client.post(url+statusCall, headers=headers, params=data4)
-except requests.exceptions.RequestException as e:
-    print(e)
-    sys.exit(1)
-parsedJson4 = r4.json()
-while parsedJson4['complete'] == "False":
-    print("Still cookin'\n")
-    sleep(1)
+while True:
+    try:
+        r4 = client.post(url+statusCall, headers=headers, params=data4)
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
+    print('Percent Complete:')
+    print(r4.json()['percentComplete'])
+    time.sleep(2)
+    completion = r4.json()['complete']
+    if completion == True:
+        break
 
 # Now that the query is done, get the results.
+print("Getting results...")
 getres = 'qryGetResults?startPos=0&numRows=5000000&reverse=false'
 data3 = { "resultID": resultID2 }
 try:
@@ -87,14 +98,15 @@ fw.write(r3.text)
 close = 'qryClose?resultID='+resultID2
 r3 = client.post(url+close, headers=headers)
 
+print("Complete")
 ########################################
 # Debugging
-print ("\n\n*** Random debug stuff ***")
-print("URL:", url+call)
-print("URL2:", url+getres)
-print('Result ID from r2:', resultID2)
-print("Status1:", r1.status_code)
-print("Status2:", r2.status_code)
-print("Status3:", r3.status_code)
-print("Status4:", r4.status_code)
-print("r4 Message: ", r4.text)
+# print ("\n\n*** Random debug stuff ***")
+# print("URL:", url+call)
+# print("URL2:", url+getres)
+# print('Result ID from r2:', resultID2)
+# print("Status1:", r1.status_code)
+# print("Status2:", r2.status_code)
+# print("Status3:", r3.status_code)
+# print("Status4:", r4.status_code)
+# print("r4 Message: ", r4.text)
